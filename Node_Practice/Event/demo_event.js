23b8d649da3cb32e2f44d813ecc85e96da8229a5
dev/Woomin <changwoomin426@gmail.com>
2024-10-01 01:35:00 +0900

    feat : mysql

---
+++ Node_Practice/Event/demo_event.js
@@ -0,0 +1,20 @@
+// const fs = require('fs');
+// const rs = fs.createReadStream('./demofile.txt');
+// rs.on('open', function () {
+//   console.log('The file is open');
+// });
+
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+
+// Create an event handler:
+const myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+// Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+// Fire the 'scream' event:
+eventEmitter.emit('scream');
+
---
+++ Node_Practice/NPM/demo_uppercase.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const uc = require('upper-case');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(uc.upperCase("Hello World!"));
+  res.end();
+}).listen(8080);
